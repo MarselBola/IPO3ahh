3bbb61e647e24b592f611cb8431f91f9040d6111
AleixBertran <58441440+AleixBertran@users.noreply.github.com>
2019-12-12 22:14:31 +0100

    Iconos a los dados predeterminados

--- app/src/main/java/com/example/crearpartida/dice/DausActivity.java
+++ app/src/main/java/com/example/crearpartida/dice/DausActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,7 +18,8 @@ import java.util.Random;
 
 public class DausActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button back, throwDau, predefinidos;
+    Button back, throwDau;
+    ImageButton predefinidos;
     Random r = new Random();
     TextView dau;
     EditText etcares;

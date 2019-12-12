11270689a186c5efa481e1036fb2e3a48814e40c
jowe-gv <58467385+jowe-gv@users.noreply.github.com>
2019-12-12 18:02:45 +0100

    - Cambios a la hora de añadir jugadores a la sala local.

--- app/src/main/java/com/example/crearpartida/localroom/LocalRoom.java
+++ app/src/main/java/com/example/crearpartida/localroom/LocalRoom.java
@@ -31,11 +31,8 @@ public class LocalRoom extends AppCompatActivity implements View.OnClickListener
         // lista de jugadores
         LinearLayout lista= findViewById(R.id.playerlist_localroom);
         int numJugadorsPartida= 0;
-        for(int i=0; (p.getNumJug() > numJugadorsPartida) && (p.getNumJug()>i); i++)
+        for(int i=0; p.getNumJug()>i; i++)
         {
-            if(!(p.getJugadors()[i].getNom().equals("empty")))
-            {
-                System.out.println(p.getJugadors()[i].getNom());
                 ConstraintLayout usuario= new ConstraintLayout(this);
                 lista.addView(usuario);
                 TextView nombre= new TextView(this);
@@ -43,7 +40,6 @@ public class LocalRoom extends AppCompatActivity implements View.OnClickListener
                 usuario.addView(nombre);
                 numJugadorsPartida++;
             }
-        }
 
         // boton iniciar partida
         botonIniciarPartida= findViewById(R.id.startgame_localroom);

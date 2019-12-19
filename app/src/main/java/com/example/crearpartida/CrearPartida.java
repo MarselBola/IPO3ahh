2447492df4f33c99aae51be499ca78b023c490a7
Joel Sola <joel.sola@estudiants.urv.cat>
2019-12-19 18:36:40 +0100

    Revert "- Diseño de CrearPartida adaptado para orientación de pantalla vertical."
    This reverts commit 875be048115dc627c0010a1d72d9ca0e1072e08d.

--- app/src/main/java/com/example/crearpartida/CrearPartida.java
+++ app/src/main/java/com/example/crearpartida/CrearPartida.java
@@ -9,10 +9,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+
 import com.example.crearpartida.clases.Globals;
 import com.example.crearpartida.clases.Partida;
+
 import com.example.crearpartida.localroom.LocalRoom;
+
 
 public class CrearPartida extends AppCompatActivity implements View.OnClickListener {
 
